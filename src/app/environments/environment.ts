15d23a8b7f0fe110b545ecf4ae8b5a8f3cc4f1e1
Nikita Borisov <kukish-2015@mail.ru>
2023-10-11 00:19:55 +0500

    first committt

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -9,8 +9,9 @@ export const environment ={
     XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
     XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
     XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-    XRapidAPIKeyHeaderValue: 'e24ad45440mshb024910e14f5cedp102ddfjsn758938dc029d'
+    XRapidAPIKeyHeaderValue: '35d36f90cemsh5063ca2cd59c3bap10d378jsn99de0c619dbd'
     // 031d9e244bmshfa2bf193edd3a73p1e95d7jsn972cdbcc1003
     // e24ad45440mshb024910e14f5cedp102ddfjsn758938dc029d
     // f6b03e17b7msh6918924b717cd0ep12fb28jsn7546500373cb
+    //35d36f90cemsh5063ca2cd59c3bap10d378jsn99de0c619dbd
 }
